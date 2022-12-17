f592fc3ae28e63fbc56924770f71a6eac23b6a87
Kyle Andrews <codingwithkyle@gmail.com>
2022-12-17 11:37:00 -0500

    ♻ Refactored -- new plugins, LSP, new colour theme, new (reduced) keymaps

---
+++ nvim/lua/config/remap.lua
@@ -0,0 +1,25 @@
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- use J and K to move selected code blocks up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- user CTRL D / U to 1/2 page jump up or down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keep search term in center of view while jumping
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- copy selected text into system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- delete line into void
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- preventing myself from entering hell
+vim.keymap.set("n", "Q", "<nop>")
--- nvim/lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd [[
-try
-  set termguicolors
-  colorscheme OceanicNext
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
