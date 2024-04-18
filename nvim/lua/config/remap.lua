0ae7335f1ba0ce6487e0fe0c821ab8e8e1eac253
Kyle Andrews <codingwithkyle@gmail.com>
2024-04-18 14:49:11 -0400

    Update remap.lua

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -27,6 +27,12 @@ vim.keymap.set("n", "Q", "<nop>")
 -- Toggle diagnostics window
 vim.keymap.set("n", "<leader>t", "<cmd>TroubleToggle<cr>")
 
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+--vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
 -- Stay in indent mode
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
