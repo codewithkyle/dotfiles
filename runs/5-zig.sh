1e5c44dac971a5bcb171f67f841d69ca993f6af8
Kyle Andrews <codingwithkyle@gmail.com>
2025-03-06 07:55:50 -0500

    Update 5-zig.sh

--- runs/5-zig.sh
+++ runs/5-zig.sh
@@ -2,5 +2,5 @@
 
 wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz
 tar -xf zig-linux-x86_64.tar.xz
-sudo mv zig-linux-x86_64 /usr/local/bin/zig
+sudo mv zig-linux-x86_64/zig /usr/local/bin/zig
 
