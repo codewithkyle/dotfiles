652ec9da0540e2d672a85aa0d651a488386f6f9c
Kyle Andrews <codingwithkyle@gmail.com>
2025-03-06 08:36:23 -0500

    Update docker.sh

--- runs/docker.sh
+++ runs/docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
